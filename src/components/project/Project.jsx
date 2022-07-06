78e86ee42a88bf596d6d4ccbc75371cb0826f8ac
kathulhur <josephkarl19@outlook.com>
2022-07-06 10:46:25 +0800

    Add project link

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 import './Project.css';
 
-const Project = ({ title, description, image }) => {
+const Project = ({ title, description, image, link }) => {
     return (
         <div className="App__projects-container_project">
             <div className="App__projects-container_project-img">
-                <img src={image}></img>
+                <img src={image} alt="inventory management system"></img>
             </div>
             <div className="App__projects-container_project-content">
-                <h2>{ title }</h2>
+                <h2><a href={link}>{ title }</a></h2>
                 <p>{ description }</p>
-                <p>Visit Repository</p>
+                <p><a href={link}>Visit Repository</a></p>
             </div>
         </div>
     );
--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -9,12 +9,14 @@ const projectsData = [
     {
         title: "Inventory Management System",
         description: "An inventory management system that has functionality such as creating, reading, updating, and deleting data records. It also contains dashboard that shows an overview of the system, allowing instant view on business insights. Also, employees can have privileges which provides confidentiality and security of data.",
-        image: inventoryManagementSystem
+        image: inventoryManagementSystem,
+        link: "https://github.com/kathulhur/ProjectLex-InventoryManagement"
     },
     {
         title: "Inventory Management System",
         description: "An inventory management system that has functionality such as creating, reading, updating, and deleting data records. It also contains dashboard that shows an overview of the system, allowing instant view on business insights. Also, employees can have privileges which provides confidentiality and security of data.",
-        image: inventoryManagementSystem
+        image: inventoryManagementSystem,
+        link: "https://github.com/kathulhur/ProjectLex-InventoryManagement"
     }
 ]
 
@@ -25,7 +27,7 @@ const Projects = () => {
             <Header text="Projects"/>
             <div className="App__projects-container">
                 {projectsData.map( (item, index) => (
-                    <Project title={item.title} description={item.description} image={inventoryManagementSystem} key={item.title + index}/>
+                    <Project title={item.title} description={item.description} image={inventoryManagementSystem} key={item.title + index} link={item.link} />
                 ))
                 }
             </div>
