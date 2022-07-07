a2479b2d07bb04950332772005ea286623c6e5c2
kathulhur <josephkarl19@outlook.com>
2022-07-07 14:45:41 +0800

    Add icon attribution

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
                 <h3>Contact</h3>
                 <p>josephkarl19@gmail.com</p>
             </div>
-
+            <p>Icon Designed by <a href="https://thenounproject.com/samsam58/">CJS</a></p>
             <p id="copyright">© 2022 Kahulhu All rights Reserved</p>
         </div>
     )
