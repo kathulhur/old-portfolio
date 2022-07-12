f93a9fe2909706da32687d129ae0602e2319321c
kathulhur <josephkarl19@outlook.com>
2022-07-13 00:10:45 +0800

    Update projects list

--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './projects.css'
 import { Header, Project } from '../../components/index'
 import inventoryManagementSystem from '../../assets/inventory-management-system.png'
-
+import urlShortener from '../../assets/url-shortener.png'
 
 const projectsData = [
     {
@@ -13,10 +13,10 @@ const projectsData = [
         link: "https://github.com/kathulhur/ProjectLex-InventoryManagement"
     },
     {
-        title: "Inventory Management System",
-        description: "An inventory management system that has functionality such as creating, reading, updating, and deleting data records. It also contains dashboard that shows an overview of the system, allowing instant view on business insights. Also, employees can have privileges which provides confidentiality and security of data.",
-        image: inventoryManagementSystem,
-        link: "https://github.com/kathulhur/ProjectLex-InventoryManagement"
+        title: "URL Shortener API",
+        description: "A URL shortener API containing functionality such as shortening URL and retrieving URL information given the code.",
+        image: urlShortener,
+        link: "https://github.com/kathulhur/url-shortener"
     }
 ]
 
@@ -27,7 +27,7 @@ const Projects = () => {
             <Header text="Projects"/>
             <div className="App__projects-container">
                 {projectsData.map( (item, index) => (
-                    <Project title={item.title} description={item.description} image={inventoryManagementSystem} key={item.title + index} link={item.link} />
+                    <Project title={item.title} description={item.description} image={item.image} key={item.title + index} link={item.link} />
                 ))
                 }
             </div>
